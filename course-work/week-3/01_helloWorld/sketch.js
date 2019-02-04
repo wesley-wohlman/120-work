dad09e13d577e80eba98f27d402ed515e667e5ce
wesley-wohlman <wesley.wohlman@umontana.edu>
2019-02-04 13:16:45 -0700

    Update from missed Week 03

---
+++ course-work/week-3/01_helloWorld/sketch.js
@@ -0,0 +1,6 @@
+function setup()
+{
+    createCanvas( 600, 400)
+    background("blue")
+    ellipse(300,200,200,100)
+}
---
+++ hw03/sketch.js
@@ -0,0 +1,10 @@
+function setup()
+{
+    createCanvas(100,100)
+    background('pink')
+}
+
+function draw()
+{
+    text("Hello World!", 20, 30)
+}
