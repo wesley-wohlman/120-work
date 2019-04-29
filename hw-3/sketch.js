ebf26dc23f480a7dd8a148316de1f0ba719361bf
wesley-wohlman <46608631+wesley-wohlman@users.noreply.github.com>
2019-04-28 18:10:00 -0600

    Created redo of Hw-03
    Restarted HW03 with a background and text

---
+++ hw-3/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    //setup canvas
+    createCanvas(600,400);
+
+    //setup background
+    background('blue');
+}
+
+function draw() {
+
+    //put in hello world
+
+    text("Hello World!", 20, 30);
+}
